a7df5b20ef576b3826289cc22c8d70d502593fec
Hockeyes33 <158685995+Hockeyes33@users.noreply.github.com>
2025-04-24 20:39:30 +0000

    clean: Christmas Lights Kata Turn on all lights

--- src/main/java/com/example/project/ChristmasLights.java
+++ src/main/java/com/example/project/ChristmasLights.java
@@ -17,10 +17,10 @@ private boolean[][] lights = new boolean[rows][columns];
 private int totalLights = 0;
 	public int TurnOnAllLights() {
 		if (totalLights < 1000000) {
-			for (int i = 0; i < rows; i++) {
-				for (int j = 0; j < columns; j++) {
-					if (!lights[i][j]){
-						lights[i][j] = true;
+			for (int rowCounter = 0; rowCounter < rows; rowCounter++) {
+				for (int columnCounter = 0; columnCounter < columns; columnCounter++) {
+					if (!lights[rowCounter][columnCounter]){
+						lights[rowCounter][columnCounter] = true;
 						totalLights++;
 					}
 				}
